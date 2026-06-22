9807fbdba514ff30ed7f29dcd4b592a9759d0e54
Muhammad Talha <talhashkeel1@gmail.com>
2026-06-22 23:52:12 +0500

    Add files via upload

---
+++ pa.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int num =1;
+	for(int i=1;i<=5;i++){
+		for(int k=1;k<=i;k++){
+			cout<<num++<<" ";
+		
+		}
+			cout<<endl;
+	}
+	return 0;
+}
